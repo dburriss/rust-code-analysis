0eadf2d26041762fefc92583452251834e3bab5b
Devon Burriss <devon@chimplab.co>
2021-10-30 15:03:07 +0200

    Get URI or PATH

--- link-check.sh
+++ link-check.sh
@@ -11,11 +11,21 @@ FILE="./rust-code-analysis-book/src/developers/new-language.md"
 printf 'Scanning %s\n' "${FILE}"
 FILE_MD_LINKS=($(grep -oE "${LINK_REG}" "${FILE}"))                   # -E extended regex, -o output only match
 echo "${FILE_MD_LINKS[0]}"
-#grep -oP "\[\K([^]]*)" <<< "${FILE_MD_LINKS[0]}"
-POSSIBLE_FILE=$(grep -oP "\[\K([^]]*)" <<< "${FILE_MD_LINKS[0]}")
-echo "${POSSIBLE_FILE}"
 # foreach file get links in it
+#grep -oP "\[\K([^]]*)" <<< "${FILE_MD_LINKS[0]}"
+URI_OR_PATH=$(grep -oP "\(\K(https?:\/\/[^()]+)" <<< "${FILE_MD_LINKS[0]}")
+echo "${URI_OR_PATH}"
 
-
+if [[ $URI_OR_PATH = http* ]]
+then
+  echo "URI"
+else
+  if [[ $URI_OR_PATH = /* ]]
+  then
+    echo "PATH"
+  fi
+fi
+  # test URI is not 404
 # for text part if starts with / and ends with .rs check if it exists
+
 # for each link check that it is not 404
